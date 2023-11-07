843dd0d6b841c48d7f0e1df01bc212fc90c2ced7
NAM GIBEOM <rlqja0523@naver.com>
2023-11-07 19:19:56 +0900

    feat: project init

---
+++ src/main/java/given53/apiversion/ApiVersionApplication.java
@@ -0,0 +1,13 @@
+package given53.apiversion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiVersionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiVersionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/given53/apiversion/ApiVersionApplicationTests.java
@@ -0,0 +1,13 @@
+package given53.apiversion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiVersionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
