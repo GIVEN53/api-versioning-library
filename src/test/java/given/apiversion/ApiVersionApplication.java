22cdeebc6811e6da1e5f60ca81821e3ec8823b6d
NAM GIBEOM <rlqja0523@naver.com>
2023-11-10 02:59:42 +0900

    refactor: delete unused files, classes and move directory

--- src/test/java/given/apiversion/ApiVersionApplication.java
+++ src/test/java/given/apiversion/ApiVersionApplication.java
@@ -1,13 +1,14 @@
-package given53.apiversion;
+package given.apiversion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import given.apiversion.autoconfigure.EnableApiVersion;
+
+@EnableApiVersion
 @SpringBootApplication
 public class ApiVersionApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ApiVersionApplication.class, args);
     }
-
 }
