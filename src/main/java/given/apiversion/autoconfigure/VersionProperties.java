fc46e63dfdbcbaccbed55e5ae293248785724b3e
NAM GIBEOM <rlqja0523@naver.com>
2023-11-09 21:46:37 +0900

    feat: add VersionProperties

---
+++ src/main/java/given/apiversion/autoconfigure/VersionProperties.java
@@ -0,0 +1,26 @@
+package given.apiversion.autoconfigure;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * {@link ConfigurationProperties} : Mapping properties that match prefix among properties to objects.
+ *
+ * @since 0.1.0
+ */
+@ConfigurationProperties(prefix = "api.version")
+public class VersionProperties {
+    /**
+     * The prefix of URI.
+     * <p>
+     * ex) /api
+     */
+    private String uriPrefix = "";
+
+    public String getUriPrefix() {
+        return uriPrefix;
+    }
+
+    public void setUriPrefix(String uriPrefix) {
+        this.uriPrefix = uriPrefix;
+    }
+}
