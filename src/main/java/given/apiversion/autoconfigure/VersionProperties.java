36919512d7438851dae0d2a9e8ef104f6368a92d
NAM GIBEOM <rlqja0523@naver.com>
2023-11-11 16:45:29 +0900

    feat: add new property

--- src/main/java/given/apiversion/autoconfigure/VersionProperties.java
+++ src/main/java/given/apiversion/autoconfigure/VersionProperties.java
@@ -2,6 +2,8 @@ package given.apiversion.autoconfigure;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import given.apiversion.core.annotation.ApiVersion;
+
 /**
  * Mapping properties that match a prefix to corresponding objects in the context of {@link ConfigurationProperties}
  *
@@ -17,8 +19,23 @@ public class VersionProperties {
      * If you set this property, the URI will be mapped to {uri-prefix}/v1.1, {uri-prefix}/v2.0
      * <p>
      * default: ""
+     *
+     * @since 0.1.0
      */
     private String uriPrefix = "";
+
+    /**
+     * This property can be useful when <strong>maintaining previous API</strong> specs that do not apply {@link ApiVersion} annotation.
+     * <p>
+     * Set this property to true, if you want to keep the old API already using the URI prefix
+     * and share it with the new API.
+     * <p>
+     * ex) previous API is /api/user -> modify it to /user, set uri-prefix to /api, set sharingUriPrefix to true.
+     * It will be mapped to /api/user and isolated from the new APIs that use {@link ApiVersion} annotation.
+     *
+     * @since 0.2.0
+     */
+    private boolean sharingUriPrefix = false;
 
     public String getUriPrefix() {
         return uriPrefix;
@@ -30,5 +47,13 @@ public class VersionProperties {
 
     public boolean isNotBlankUriPrefix() {
         return uriPrefix != null && !uriPrefix.isBlank();
+    }
+
+    public boolean isSharingUriPrefix() {
+        return sharingUriPrefix;
+    }
+
+    public void setSharingUriPrefix(boolean sharingUriPrefix) {
+        this.sharingUriPrefix = sharingUriPrefix;
     }
 }
